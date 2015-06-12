4795ad36cd8596b133f8e0cc4bff108902cbd89f
Shane Kumpf <sakserv@sakserv.org>
2015-06-12 05:59:40 -0600

    updated beeline wrapper and sql scripts

--- beeline_wrapper.sh
+++ beeline_wrapper.sh
@@ -5,6 +5,6 @@ if [ -z "$1" ]; then
   exit 1
 fi
 database=$1
-shift $@
+shift
 
 beeline -u jdbc:hive2://localhost:10000/$database -d org.apache.hive.jdbc.HiveDriver -n $(id -un) $@
--- sql/sql/insert_into_orc.sql
+++ sql/sql/insert_into_orc.sql
@@ -1,5 +1,6 @@
 set hive.enforce.bucketing=true;
 set hive.exec.dynamic.partition.mode=nonstrict;
-INSERT INTO TABLE uidata_orc PARTITION(dt) 
+use honeywell_acs_hch_poc;
+INSERT INTO TABLE uidata_orc PARTITION(dt)
 SELECT *,to_date(from_unixtime(unix_timestamp(timestamp,'MM/dd/yyyy h:mm:ss aa'))) AS dt FROM uidata_staging
 WHERE deviceid != 'DeviceID';
