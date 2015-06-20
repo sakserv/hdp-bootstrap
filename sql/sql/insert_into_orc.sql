6996471433fd8a76c9f3052e4004db05be347a64
Shane Kumpf <sakserv@sakserv.org>
2015-06-20 06:07:30 -0600

    additional tuning of orc load

--- sql/sql/insert_into_orc.sql
+++ sql/sql/insert_into_orc.sql
@@ -1,7 +1,10 @@
 set hive.enforce.bucketing=true;
 set hive.exec.dynamic.partition.mode=nonstrict;
+set hive.exec.parallel=true;
 set hive.tez.container.size=16384;
 set hive.tez.java.opts=-server -Xmx14336m -Djava.net.preferIPv4Stack=true -XX:NewRatio=8 -XX:+UseNUMA -XX:+UseParallelGC -XX:+PrintGCDetails -verbose:gc -XX:+PrintGCTimeStamps;
+set tez.runtime.io.sort.mb=2047;
+set hive.execution.engine=tez;
 use honeywell_acs_hch_poc;
 INSERT INTO TABLE uidata_orc PARTITION(dt)
 SELECT *,to_date(from_unixtime(unix_timestamp(timestamp,'MM/dd/yyyy h:mm:ss aa'))) AS dt FROM uidata_staging
