1ff682a14b05ae9545690b16fb4274e25e8ec872
Shane Kumpf <sakserv@sakserv.org>
2015-06-27 12:25:18 -0600

    copy hive backups to other nodes

--- hive_backups.sh
+++ hive_backups.sh
@@ -10,9 +10,12 @@ SCRIPT_DIR=$(cd `dirname $0` && pwd)
 #
 # Variables
 #
+SSH_PRIVATE_KEY_PATH=/root/.ssh/id_hdp
+SSH_ARGS="-q -i $SSH_PRIVATE_KEY_PATH -o StrictHostKeyChecking=no"
 DT=$(date +"%Y-%m-%d")
 LOG_DIR=/data/backups/hive
 DB_LIST="hive"
+SVR_COPY_LIST="hdpclustermstr1.cloudapp.net hdpclustermstr2.cloudapp.net"
 
 
 #
@@ -50,6 +53,13 @@ for db in $DB_LIST; do
   echo -e "\n#### Running the Hive database backup for DB: $db"
   OUTFILE=$LOG_DIR/hive-db-${db}-backup.${DT}
   mysqldump $db >$OUTFILE
+  echo "SUCCESS"
+done
+
+for server in $SVR_COPY_LIST; do
+  echo -e "\n#### Copying backup to $server"
+  ssh $SSH_ARGS $server "mkdir -p $LOG_DIR"
+  scp $SSH_ARGS $LOG_DIR/hive-db-*-backup.${DT} $server:$LOG_DIR/
   echo "SUCCESS"
 done
 
