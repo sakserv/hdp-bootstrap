5d9a77a8660f0eedcce5d68ec52ac4f472d644ec
Shane Kumpf <sakserv@sakserv.org>
2015-06-27 10:30:04 -0600

    updated usage slightly

--- onboard_user.sh
+++ onboard_user.sh
@@ -25,7 +25,7 @@ QUOTA_BYTES=$(echo $(( QUOTA_GB * 1024 * 1024 * 1024 )))
 usage() {
   echo "USAGE: $SCRIPT_NAME -u <user id> -a /tmp/allnodes -i <uid> -g <gid> [-s]"
   echo -e "\t-u - the user id to create"
-  echo -e "\t-a - file containing a list of all nodes, one per line"
+  echo -e "\t-a - file containing a list of nodes where the user and group will be created, one per line"
   echo -e "\t-i - UID of the user, this must match across all cluster members"
   echo -e "\t-g - GID of the user, this must match across all cluster members"
   echo -e "\t-s - skip adding user to HDFS - optional"
